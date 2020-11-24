10a214aaebcffe5c7ef2d2bd593ff7331ff955d3
superuser992002 <Zalarus.pbc168@gmail.com>
2020-11-24 23:30:11 +0700

    deleting

--- mongo-seed/import.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-mongoimport --host database --db database_devC --collection user --type json --file /mongo-seed/init-db1.json --jsonArray
-mongoimport --host database --db database_devC -- collection cache --type json --file /mongo-seeed/init-db2.json --jsonArray
--- server/firebase/index.js
+++
@@ -1,9 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("../config/fbServiceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://devcteam01.firebaseio.com"
-});
-module.exports = admin;
